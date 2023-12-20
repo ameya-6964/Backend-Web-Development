75e1533a3fd8eeca4dee5a858c894c141e6ce1e2
ameya-6964 <belvalkar7@gmail.com>
2023-12-21 02:34:02 +0530

    Created Basic Server Using express.js

---
+++ express/first-express-application/app.js
@@ -0,0 +1,22 @@
+//! express is not built in module for Creating s=wen Servers
+const express = require("express");
+
+//! For Using Methods Of Express (naming convnetions)
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    `<a href="/contact">Contact Us</a> <br> <a href="/about">About Us</a> `
+  );
+});
+app.get("/contact", (req, res) => {
+  res.send(`<h1>Contact Us Page</h1>`);
+});
+app.get("/about", (req, res) => {
+  res.send(`<h1>About Us Page</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server Started On Port ${port}`);
+});
