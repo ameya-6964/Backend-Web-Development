2a6c6dafc1a8aab6c6d18b1a79e54345c3453f0e
ameya-6964 <belvalkar7@gmail.com>
2023-12-21 01:55:27 +0530

    Installed casual from npm website and integrated inside app.js

--- Basics/app.js
+++ Basics/app.js
@@ -1,8 +1,13 @@
 //! Creating Server With Core Http Module
 const http = require("http");
+const casual = require("casual");
+
 const port = 3000;
+const sentence = casual._sentence();
+console.log(sentence);
+
 const server = http.createServer((req, res) => {
-  res.end("<h1>First Server</h1>");
+  res.end(`<h1>${sentence}</h1>`);
 });
 
 server.listen(port, () => {
