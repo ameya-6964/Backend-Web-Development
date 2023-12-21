d09b2d66bbe9bbcb7fd2a229589e83dc99149248
ameya-6964 <belvalkar7@gmail.com>
2023-12-21 22:23:24 +0530

    Created POST Login Route With ejs and Express

--- express/post-requests/app.js
+++ express/post-requests/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "view"));
 app.set("view engine", "ejs");
@@ -11,6 +14,23 @@ app.get("/", (req, res) => {
 });
 app.get("/login", (req, res) => {
   res.render("login");
+});
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+app.get("/404", (req, res) => {
+  res.render("404");
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (username === "ameya" && password === "123456") {
+    res.redirect("/dashboard");
+  } else {
+    res.redirect("/404");
+  }
 });
 
 app.listen(port, () => {
