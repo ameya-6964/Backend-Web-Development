ba822fc070c89948743a4d5545b00f3d5dd6a3ac
ameya-6964 <belvalkar7@gmail.com>
2023-12-21 16:44:16 +0530

    Used Loops With ejs Loops

---
+++ express/ejsloops/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/questions", (req, res) => {
+  let questions = [
+    {
+      title: "What is Node.js",
+      user: "Ameya",
+      votes: "10",
+    },
+    {
+      title: "What is Express.js",
+      user: "John",
+      votes: "8",
+    },
+    {
+      title: "What is EJS",
+      user: "Mike",
+      votes: "5",
+    },
+  ];
+  res.render("index", { questions: questions });
+});
+
+app.listen(port, () => {
+  console.log(`Server Started On Port ${port}`);
+});
